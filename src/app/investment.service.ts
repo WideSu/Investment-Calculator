06d216561b6f9b42c220face0f06ac7a54e39922
Anni HUANG <anni.huang@rockwellautomation.com>
2024-07-28 21:08:24 +0800

    Using signals in service

--- src/app/investment.service.ts
+++ src/app/investment.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from "@angular/core";
+import { Injectable, signal } from "@angular/core";
 import type { InvestmentInput } from "./investment-input.model";
 
 @Injectable({providedIn: 'root'})
 export class InvestmentService {
-    resultData? : {
-        year: number,
-        interest: number,
-        valueEndOfYear: number,
-        annualInvestment: number,
-        totalInterest: number,
-        totalAmountInvested: number,
-      }[]
+    resultData = signal<{
+      year: number,
+      interest: number,
+      valueEndOfYear: number,
+      annualInvestment: number,
+      totalInterest: number,
+      totalAmountInvested: number,
+    }[] | undefined>(undefined);
     calculateInvestmentResults(data:InvestmentInput) {
         const {
           initialInvestment, 
@@ -36,7 +36,7 @@ export class InvestmentService {
             totalAmountInvested: initialInvestment + annualInvestment * year,
           });
         }
-        this.resultData = annualData;
+        this.resultData.set(annualData);
         // this.resultsData.set(annualData);
       }
 
