ddd539ffb3702fbb994297f7a0c8ab880f5016d9
Anni HUANG <anni.huang@rockwellautomation.com>
2024-07-28 20:44:08 +0800

    Initial push

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './app/header/header.component'
+import { UserInputComponent } from "./user-input/user-input.component";
+// import type { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  // resultsData = signal<{
+  //   year: number,
+  //   interest: number,
+  //   valueEndOfYear: number,
+  //   annualInvestment: number,
+  //   totalInterest: number,
+  //   totalAmountInvested: number,
+  // }[] | undefined>(undefined);
+
+}
